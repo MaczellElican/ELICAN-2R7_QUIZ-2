cfab2768197a13a1d26cff74fe3c10efc4c61b9c
MaczellElican <125783835+MaczellElican@users.noreply.github.com>
2023-04-01 19:55:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function calc(){
+    let operator = document.getElementById("operator").value;
+    let numberOne = document.getElementById("numberOne").value;
+    let numberTwo = document.getElementById("numberTwo").value;
+
+    numberOne = Number(numberOne);
+    numberTwo = Number(numberTwo);
+
+    let answer = document.getElementById("answer");
+    
+    switch(operator){
+        case '+':
+            answer.innerHTML = numberOne + numberTwo;
+            break;
+        case '-':
+           answer.innerHTML = numberOne - numberTwo;
+            break;
+        case '*':
+           answer.innerHTML = numberOne * numberTwo;
+            break;
+        case '/':
+            answer.innerHTML = numberOne / numberTwo;
+            break;
+        default:
+           answer.style.color = 'red';
+           answer.innerHTML = "Invalid Input";
+    }
+}
